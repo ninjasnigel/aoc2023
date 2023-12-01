7545647c6ab105148d70ce2a23c79e9b092fa179
David Laessker <davidlaessker@gmail.com>
2023-12-01 16:17:39 +0100

    more readable, I guess...

--- day1/sol.py
+++ day1/sol.py
@@ -2,7 +2,11 @@ nums = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven":
 get_num = lambda s, i: next((nums.get(s[i:i+l]) for l in (3, 4, 5) if i + l <= len(s) and s[i:i+l] in nums), '')
 
 #sol1
-with open("data.aoc", "r") as file: total_sum = sum([int(n[0] + n[-1]) for n in ["".join(c for c in line if c.isdigit()) for line in file.readlines()]]); print(total_sum)
+with open("data.aoc", "r") as file: 
+    total_sum = sum([int(n[0] + n[-1]) for n in \
+                ["".join(c for c in line if c.isdigit()) for line in file.readlines()]]); print(total_sum)
 
 #sol2
-with open("data.aoc", "r") as file: total_sum = sum([int(n[0] + n[-1]) for n in ["".join(str(c) if c.isdigit() else str(get_num(line, i)) for i, c in enumerate(line)) for line in file.readlines()]]); print(total_sum)
+with open("data.aoc", "r") as file: 
+    total_sum = sum([int(n[0] + n[-1]) for n in ["".join(str(c) if c.isdigit() else \
+                str(get_num(line, i)) for i, c in enumerate(line)) for line in file.readlines()]]); print(total_sum)
